ee1a85c273a721bca7f1e7b03613517f83ae12ac
Igor Zevnik <igor.v.zevnik@gmail.com>
2023-12-29 17:00:46 +0100

    test: testing workflow

--- .github/prepare-release.sh
+++ .github/prepare-release.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then printf "\033[0;31mPlease run as root\033[0m"
-  exit
-fi
-
 version=$(git describe --tags)
+
+echo "Preparing verion $version"
 
 error() {
     ret=$(cat)
@@ -19,26 +16,20 @@ error() {
 
 printf "\033[1;33mCreating release\n\033[0m"
 
-test -d $version
-if [ $? -eq 0 ]; then
-    rev=$(stat $version -c %n-%W)
-    mv "$version" "$rev"
-fi
-
 exec 3>&1
 {
-    mkdir $version
+    mkdir bin
 
-    rsync -a ../SecTrails.sh $version/SecTrails-linux-arm64/
-    rsync -a ../SecTrails.sh $version/SecTrails-linux-amd64/
-    rsync -a ../jq-linux-amd64 $version/SecTrails-linux-amd64/
-    rsync -a ../jq-linux-arm64 $version/SecTrails-linux-arm64/
+    rsync -a SecTrails.sh bin/SecTrails-linux-arm64/
+    rsync -a SecTrails.sh bin/SecTrails-linux-amd64/
+    rsync -a jq-linux-amd64 bin/SecTrails-linux-amd64/
+    rsync -a jq-linux-arm64 bin/SecTrails-linux-arm64/
 
-    zip $version/SecTrails-linux-arm64.zip $version/SecTrails-linux-arm64 
-    zip $version/SecTrails-linux-amd64.zip $version/SecTrails-linux-amd64
+    zip bin/SecTrails-linux-arm64.zip bin/SecTrails-linux-arm64 
+    zip bin/SecTrails-linux-amd64.zip bin/SecTrails-linux-amd64
 
-    sha256sum $version/SecTrails-linux-arm64.zip > $version/sha256sums
-    sha256sum $version/SecTrails-linux-amd64.zip >> $version/sha256sums
+    sha256sum bin/SecTrails-linux-arm64.zip > bin/sha256sums
+    sha256sum bin/SecTrails-linux-amd64.zip >> bin/sha256sums
 } 2>&1 1>&3  | error
 
 
